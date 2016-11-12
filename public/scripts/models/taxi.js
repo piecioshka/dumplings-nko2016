6a537ca6d105f7fc4347e89f11f5ea4cce050eb1
ksyrytczyk <krzysztof.syrytczyk@gmail.com>
2016-11-12 12:52:55 +0100

    Add keyboard support for player movement

--- public/scripts/models/taxi.js
+++ public/scripts/models/taxi.js
@@ -1,14 +1,51 @@
 let CONSTANTS = require('../constants/game');
 
+const TAXI_CONSTANTS = {
+    MOVE_BY: {
+        up: 10,
+        down: 10,
+        left: 10,
+        right: 10
+    }
+}
+
 class Taxi extends Phaser.Sprite {
+    cursors = null;
+
     constructor(game) {
         super(game, 0, 0, 'taxi', 1);
         game.add.existing(this);
+
+        this.setupControls();
     }
 
     move(x, y) {
         this.x = x * CONSTANTS.TILE_WIDTH;
         this.y = y * CONSTANTS.TILE_HEIGHT;
+    }
+
+    setupControls() {
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+    }
+
+    updateMove() {
+        let cursors = this.cursors;
+
+        if (cursors.up.isDown) {
+            this.y -= TAXI_CONSTANTS.MOVE_BY.up;
+        }
+
+        if (cursors.down.isDown) {
+            this.y += TAXI_CONSTANTS.MOVE_BY.down;
+        }
+
+        if (cursors.left.isDown) {
+            this.x -= TAXI_CONSTANTS.MOVE_BY.left;
+        }
+
+        if (cursors.right.isDown) {
+            this.x += TAXI_CONSTANTS.MOVE_BY.right;
+        }
     }
 }
 
