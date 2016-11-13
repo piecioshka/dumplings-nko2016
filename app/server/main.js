c2bcbe0227eb6a81e20f0024ce27da67d212436f
Piotr Kowalski <piecioshka@gmail.com>
2016-11-13 17:31:47 +0100

    Add sudo

--- app/server/main.js
+++ app/server/main.js
@@ -3,8 +3,7 @@
 var express = require('express');
 var app = express();
 
-// app.set('port', (process.env.PORT || 5000));
-app.set('port', 5124);
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/../../public'));
 
 var server = app.listen(app.get('port'), function () {
