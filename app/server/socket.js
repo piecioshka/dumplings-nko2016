56d2b9a00a3b5c6a2375ad1db85b38598a801204
ksyrytczyk <krzysztof.syrytczyk@gmail.com>
2016-11-13 14:00:09 +0100

    More es6

--- app/server/socket.js
+++ app/server/socket.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var EVENTS = require('../server/constants/events');
-var PlayersCollection = require('./players-collection');
-var PassengersCollection = require('./passengers-collection');
+let EVENTS = require('../server/constants/events');
+let PlayersCollection = require('./players-collection');
+let PassengersCollection = require('./passengers-collection');
 
-module.exports = function (server) {
-    var io = require('socket.io')(server);
-    var playersCollection = new PlayersCollection();
-    var passengersCollection = new PassengersCollection();
+module.exports = (server) => {
+    let io = require('socket.io')(server);
+    let playersCollection = new PlayersCollection();
+    let passengersCollection = new PassengersCollection();
 
-    io.on('connection', function (socket) {
+    io.on('connection', (socket) => {
         console.log('a user connected');
 
         let me = null;
@@ -20,24 +20,24 @@ module.exports = function (server) {
             me = null;
         }
 
-        socket.on('error', function () {
+        socket.on('error', () => {
             console.log('error occur: ' + JSON.stringify(me));
             removePlayer();
         });
 
-        socket.on('disconnect', function () {
+        socket.on('disconnect', () => {
             console.log('user disconnected: ' + JSON.stringify(me));
             removePlayer();
         });
 
-        socket.on(EVENTS.SETUP_PLAYER, function (player) {
+        socket.on(EVENTS.SETUP_PLAYER, (player) => {
             console.log('setup player: message: ' + JSON.stringify(player));
             playersCollection.add(player);
             io.emit(EVENTS.SETUP_PLAYER, playersCollection.players);
             me = player;
         });
 
-        socket.on(EVENTS.SET_PASSENGERS, function (coords) {
+        socket.on(EVENTS.SET_PASSENGERS, (coords) => {
             console.log('setup passengers: message: ' + JSON.stringify(coords));
             passengersCollection.setCoords(coords);
             passengersCollection.setThreshold(200);
@@ -45,7 +45,7 @@ module.exports = function (server) {
             io.emit(EVENTS.SET_PASSENGERS, playersCollection.passengers);
         });
 
-        socket.on(EVENTS.MOVE_PLAYER, function (player) {
+        socket.on(EVENTS.MOVE_PLAYER, (player) => {
             // console.log('move player: message: ' + JSON.stringify(player));
             io.emit(EVENTS.MOVE_PLAYER, player);
             playersCollection.update(player.id, player);
