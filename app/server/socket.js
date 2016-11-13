b69231e80cb136f91ea4aa9a5fae3cc700c01e48
ksyrytczyk <krzysztof.syrytczyk@gmail.com>
2016-11-13 13:57:45 +0100

    Rename socket event naming

--- app/server/socket.js
+++ app/server/socket.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var SOCKET = require('../server/constants/socket');
+var EVENTS = require('../server/constants/events');
 var PlayersCollection = require('./players-collection');
 var PassengersCollection = require('./passengers-collection');
 
@@ -15,7 +15,7 @@ module.exports = function (server) {
         let me = null;
 
         function removePlayer() {
-            io.emit(SOCKET.DISCONNECT_PLAYER, me);
+            io.emit(EVENTS.DISCONNECT_PLAYER, me);
             playersCollection.remove(me);
             me = null;
         }
@@ -30,24 +30,24 @@ module.exports = function (server) {
             removePlayer();
         });
 
-        socket.on(SOCKET.SETUP_PLAYER, function (player) {
+        socket.on(EVENTS.SETUP_PLAYER, function (player) {
             console.log('setup player: message: ' + JSON.stringify(player));
             playersCollection.add(player);
-            io.emit(SOCKET.SETUP_PLAYER, playersCollection.players);
+            io.emit(EVENTS.SETUP_PLAYER, playersCollection.players);
             me = player;
         });
 
-        socket.on(SOCKET.SET_PASSENGERS, function (coords) {
+        socket.on(EVENTS.SET_PASSENGERS, function (coords) {
             console.log('setup passengers: message: ' + JSON.stringify(coords));
             passengersCollection.setCoords(coords);
             passengersCollection.setThreshold(200);
             passengersCollection.generate();
-            io.emit(SOCKET.SET_PASSENGERS, playersCollection.passengers);
+            io.emit(EVENTS.SET_PASSENGERS, playersCollection.passengers);
         });
 
-        socket.on(SOCKET.MOVE_PLAYER, function (player) {
+        socket.on(EVENTS.MOVE_PLAYER, function (player) {
             // console.log('move player: message: ' + JSON.stringify(player));
-            io.emit(SOCKET.MOVE_PLAYER, player);
+            io.emit(EVENTS.MOVE_PLAYER, player);
             playersCollection.update(player.id, player);
         });
     });
